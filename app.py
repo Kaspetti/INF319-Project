c539cb67cdbdac8e827bc04d51085f34aae4bba3
Kaspar Moberg <kaspeti123@gmail.com>
2024-08-29 01:42:28 +0200

    added get-networks endpoint

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from data import read_data, get_geometric_center, generate_network
 
 
 app = Flask(__name__)
@@ -7,3 +8,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/api/get-network")
+def get_network():
+    lines = read_data("2024082712", 0, 0)
+    centers = [get_geometric_center(line["coords"]) for line in lines]
+
+    return jsonify(generate_network(centers, 1000))
