bdbb6a6faff4ff7977ea0367659dba1222edd70f
Kaspar Moberg <kaspar@kasparm.home>
2024-09-05 11:18:34 +0200

    feat: added get-coords endpoint

--- sigma/main.py
+++ sigma/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
-from data import read_data, get_geometric_center, generate_network
+from data import read_data, generate_network
 
 
 app = FastAPI()
@@ -41,6 +41,21 @@ def get_network(sim_start: str = "2024082712",
                 dist_threshold: int = 500):
     lines = []
     for i in range(50):
-        lines += read_data(sim_start, i, time_offset)
+        lines.extend(read_data(sim_start, i, time_offset))
 
     return generate_network(lines, dist_threshold)
+
+
+@app.get("/get-coords")
+def get_coords(sim_start: str = "2024082712",
+               time_offset: int = 0):
+    lines = []
+    for i in range(50):
+        ls = read_data(sim_start, i, time_offset)
+        for i in range(len(ls)):
+            ls[i]["coords"] = [{"lat": coord[0], "lon": coord[1]}
+                               for coord in ls[i]["coords"]]
+
+        lines.extend(ls)
+
+    return lines
