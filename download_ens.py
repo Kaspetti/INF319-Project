61e13740d4106334be3c6feacd13a00eebda8fae
Kaspar Moberg <kaspeti123@gmail.com>
2024-11-22 12:26:03 +0100

    feat: added improved download script

--- download_ens.py
+++ download_ens.py
@@ -1,21 +1,38 @@
 import urllib.request
 import os
+import sys
+
+
+if len(sys.argv) < 2:
+    print("Line type not specified. Must be one of 'mta' and 'jet'")
+    exit()
+
+typ = sys.argv[1]
+if typ not in {"mta", "jet"}:
+    print(f"Invalid line type '{typ}'. Must be one of 'mta' and 'jet'")
+    exit()
 
 
 def filename(x, d):
-    return f"ec.ens_{x}.{d}.sfc.mta.nc"
+    if typ == "jet":
+        return f"ec.ens_{x}.{d}.pv2000.jetaxis.nc"
+    else:
+        return f"ec.ens_{x}.{d}.sfc.mta.nc"
 
 
-date = "2024082712"
+dates = ["2024101900"]
 
-if not os.path.exists(f"./{date}"):
-    os.mkdir(f"./{date}")
 
-for i in range(50):
-    f = filename(f"{i:02d}", date)
-    print(f"Downloading '{f}'")
+for date in dates:
+    folder_path = f"./data/{'jet' if typ == 'jet' else 'mta'}/{date}"
+    if not os.path.exists(folder_path):
+        os.mkdir(folder_path)
 
-    urllib.request.urlretrieve(
-            f"https://iveret.gfi.uib.no/mtaens/{f}",
-            f"./{date}/{f}"
-    )
+    for i in range(50):
+        f = filename(f"{i:02d}", date)
+        print(f"Downloading '{f}'")
+
+        urllib.request.urlretrieve(
+            f"https://iveret.gfi.uib.no/{'jetens' if typ == 'jet' else 'mtaens'}/{f}",
+            f"{folder_path}/{f}",
+        )
